b9d5dce7b1ea2092560de8abe96b478608441108
JASSADHAMMI <jassadhammi@gmail.com>
2026-05-23 16:45:36 +0530

    add

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { CookieService } from 'ngx-cookie-service'; // 👈 Eh import karo
-import { provideAnimations } from '@angular/platform-browser/animations'; // 👈 Added import
+import { CookieService } from 'ngx-cookie-service';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './core/services/interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimations(), // 👈 Zaroori hai
+    provideAnimations(),
     provideToastr({
       timeOut: 3000,
       positionClass: 'toast-top-right',
@@ -16,6 +18,7 @@ export const appConfig: ApplicationConfig = {
     }),
     provideBrowserGlobalErrorListeners(),
     CookieService,
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
